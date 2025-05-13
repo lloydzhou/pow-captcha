db2946c9269a2ce4a5faf4d6dd72657065f47b3b
lloydzhou <lloydzhou@qq.com>
2025-05-13 16:24:25 +0800

    update

---
+++ public/sdk.js
@@ -0,0 +1,34 @@
+(function() {
+    const scripts = document.getElementsByTagName('script');
+    const currentScript = scripts[scripts.length - 1];
+    const host = new URL(currentScript.src).origin;
+    console.log('host', host);
+    window.POW = function(options = {}) {
+        const endpoint = options.endpoint || host;
+        const iframe = document.createElement('iframe');
+        let callback = null;
+        let timeoutId = null;
+        return new Promise((resolve, reject) => {
+            callback = function(event) {
+                if (event.data.type === 'pow') {
+                    resolve(event.data.token);
+                }
+            }
+            timeoutId = setTimeout(() => {
+                reject(new Error('POW timeout'));
+            }, options.timeout || 10000);
+            window.addEventListener('message', callback);
+            iframe.src = endpoint;
+            iframe.style.display = 'none';
+            document.body.appendChild(iframe);
+        }).finally(() => {
+            document.body.removeChild(iframe);
+            if (callback) {
+                window.removeEventListener('message', callback);
+            }
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+        });
+    }
+})()

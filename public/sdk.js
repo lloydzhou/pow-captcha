c6dab475962f43f0ad8aec1238072e76e3fa0169
lloydzhou <lloydzhou@qq.com>
2025-05-20 16:22:45 +0800

    update

--- public/sdk.js
+++ public/sdk.js
@@ -1,4 +1,4 @@
-// terser public/sdk.js -m --mangle-props > public/sdk.min.js
+// terser public/sdk.js -m --mangle-props reserved="[render]" > public/sdk.min.js
 (function() {
     const TIMEOUT = 30000; // 30 seconds
     const scripts = document.getElementsByTagName('script');
@@ -33,46 +33,52 @@
         });
     };
 
-    function initializeAutoCaptcha() {
-        const captchaDivs = document.querySelectorAll('.pow-captcha');
-        captchaDivs.forEach(div => {
-            const hiddenInput = document.createElement('input');
-            hiddenInput.type = 'hidden';
-            hiddenInput.name = 'pow-captcha-token'; // 根据要求设置name
-            div.appendChild(hiddenInput);
-
-            const powOptions = {};
-            if (div.dataset.timeout) {
-                const timeoutValue = parseInt(div.dataset.timeout, TIMEOUT);
-                if (!isNaN(timeoutValue) && timeoutValue > 0) {
-                    powOptions.timeout = timeoutValue;
-                }
+    function executeCallback(callbackName, ...args) {
+        if (callbackName && typeof window[callbackName] === 'function') {
+            try {
+                return window[callbackName](...args);
+            } catch (e) {
+                console.error('POW Captcha: Error executing callback function "' + callbackName + '".', e);
             }
-            // 可以从 data-* 属性中提取其他 POW 选项
+        } else if (callbackName) {
+            console.warn('POW Captcha: Callback function "' + callbackName + '" not found or not a function.');
+        }
+    }
 
-            function executeCallback(callbackName, ...args) {
-                if (callbackName && typeof window[callbackName] === 'function') {
-                    try {
-                        return window[callbackName](...args);
-                    } catch (e) {
-                        console.error('POW Captcha: Error executing callback function "' + callbackName + '".', e);
-                    }
-                } else if (callbackName) {
-                    console.warn('POW Captcha: Callback function "' + callbackName + '" not found or not a function.');
-                }
+    const render = function(contaner, options = {}) {
+        const initializedAttribute = 'data-pow-initialized';
+        if (contaner.getAttribute(initializedAttribute) === 'true') {
+            console.warn('POW Captcha: This element has already been initialized.');
+            return;
+        }
+        const hiddenInput = document.createElement('input');
+        hiddenInput.type = 'hidden';
+        hiddenInput.name = 'pow-captcha-token'; // 根据要求设置name
+        contaner.appendChild(hiddenInput);    
+        const dataset = contaner.dataset;
+        if (dataset.timeout) {
+            const timeoutValue = parseInt(dataset.timeout, TIMEOUT);
+            if (!isNaN(timeoutValue) && timeoutValue > 0) {
+                options.timeout = timeoutValue;
             }
-
-            pow(powOptions)
-                .then(token => {
-                    hiddenInput.value = token;
-                    executeCallback(div.dataset.callback, token);
-                })
-                .catch(error => {
-                    executeCallback(div.dataset.errorCallback, error);
-                });
+        }
+        if (dataset.endpoint) {
+            options.endpoint = dataset.endpoint;
+        }
+        contaner.setAttribute(initializedAttribute, 'true');
+        pow(options).then(token => {
+            hiddenInput.value = token;
+            executeCallback(dataset.callback, token);
+        }).catch(error => {
+            executeCallback(dataset.errorCallback, error);
         });
     }
 
+    function initializeAutoCaptcha() {
+        const captchaDivs = document.querySelectorAll('.pow-captcha');
+        captchaDivs.forEach(render);
+    }
+    window.POW['render'] = render; // 允许外部调用
     if (document.readyState === 'loading') {
         document.addEventListener('DOMContentLoaded', initializeAutoCaptcha);
     } else {
